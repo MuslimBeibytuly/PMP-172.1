20cf5bc54576d147133a39f97595e31c53bbc260
muslimbeibytuly <muslimbeibytuly@gmail.com>
2018-05-12 12:26:45 +0600

    Const method

--- OwnStringImplementation/String.h
+++ OwnStringImplementation/String.h
@@ -5,11 +5,10 @@ class String
 	char * data;
 	size_t size;
 public:
-	String();
-	String(const String &);
+	explicit String();
+	explicit String(const String &);
 	~String();
 	size_t count() const;
 	void print() const;
 	void addCharacter(const char &);
 };
-
